1313ab1c0b98eb63b5a74fb33af968c7660960bc
Zaynab Abd El Nabi <zaynab.abdelnabi@gmail.com>
2021-03-30 19:09:25 +0300

    main page

--- src/js/index.js
+++ src/js/index.js
@@ -3,6 +3,17 @@ import '@laylazi/bootstrap-rtl/dist/css/bootstrap-rtl.min.css';
 import 'jquery/dist/jquery.min.js';
 import 'popper.js/dist/popper.min.js';
 import '../sass/style.scss';
+import '@fortawesome/fontawesome-free/js/all';
 
-
-console.log("hello webpack");
+$(function(){
+    $('.thumbnail1').hover(function(){
+        $(this).find('.project-category').hide();
+        $(this).find('.caption1').slideDown(250);
+    },
+    function(){
+        $(this).find('.caption1').slideUp(250);
+        $(this).find('.project-category').show();
+    }
+    );
+});
+$('.year').html(new Date().getFullYear());
--- webpack.config.js
+++ webpack.config.js
@@ -8,6 +8,7 @@ module.exports = {
   entry: './src/js/index.js',
 
   output: {
+    publicPath: '/',
     path: path.resolve(__dirname, 'build'),
     filename: 'js/bundle.js',
   },
@@ -15,7 +16,7 @@ module.exports = {
     contentBase: path.join(__dirname, 'build'),
     port: 9001,
     writeToDisk: true,
-    stats :'errors-only',
+    stats: 'errors-only',
     open: true,
   },
 
@@ -26,21 +27,59 @@ module.exports = {
         use: [
           {
             loader: MiniCssExtractPlugin.loader,
+            options: {
+              publicPath : '../',
+            }
           },
           "css-loader",
           "sass-loader",
         ],
       },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: "images",
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf|svg)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: "fonts",
+            },
+          },
+        ],
+      },
+      {
+        test: /\.html$/i,
+        loader: 'html-loader',
+      },
+      {
+        test: require.resolve("jquery"),
+        loader: "expose-loader",
+        options: {
+          exposes: ["$", "jQuery"],
+        },
+      },
     ],
   },
 
   plugins: [
-    
+
     new HtmlWebpackPlugin({
       template: './src/index.html',
       filename: 'index.html',
     }),
 
-    new MiniCssExtractPlugin({filename: 'css/style.css',}),
+    new MiniCssExtractPlugin({ filename: 'css/style.css', }),
   ],
 };
