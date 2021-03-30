98dbf6daecf1bf00fafb586edabf9eaad01edf8d
Zaynab Abd El Nabi <zaynab.abdelnabi@gmail.com>
2021-03-31 01:00:09 +0300

    projects page with details

--- src/js/index.js
+++ src/js/index.js
@@ -5,15 +5,81 @@ import 'popper.js/dist/popper.min.js';
 import '../sass/style.scss';
 import '@fortawesome/fontawesome-free/js/all';
 
-$(function(){
-    $('.thumbnail1').hover(function(){
+$(function () {
+    $('.thumbnail1').hover(function () {
         $(this).find('.project-category').hide();
         $(this).find('.caption1').slideDown(250);
     },
-    function(){
-        $(this).find('.caption1').slideUp(250);
-        $(this).find('.project-category').show();
-    }
+        function () {
+            $(this).find('.caption1').slideUp(250);
+            $(this).find('.project-category').show();
+        }
     );
+
+
+
+
+    let modalId = $('#image-gallery');
+
+
+    loadGallery(true, 'a.thumbnail');
+
+    //This function disables buttons when needed
+    function disableButtons(counter_max, counter_current) {
+        $('#show-previous-image, #show-next-image')
+            .show();
+        if (counter_max === counter_current) {
+            $('#show-next-image')
+                .hide();
+        } else if (counter_current === 1) {
+            $('#show-previous-image')
+                .hide();
+        }
+    }
+
+    function loadGallery(setIDs, setClickAttr) {
+        let current_image,
+            selector,
+            counter = 0;
+
+        $('#show-next-image, #show-previous-image')
+            .on('click',function () {
+                if ($(this)
+                    .attr('id') === 'show-previous-image') {
+                    current_image--;
+                } else {
+                    current_image++;
+                }
+
+                selector = $('[data-image-id="' + current_image + '"]');
+                updateGallery(selector);
+            });
+
+        function updateGallery(selector) {
+            let $sel = selector;
+            current_image = $sel.data('image-id');
+            $('#image-gallery-title')
+                .text($sel.data('title'));
+            $('#image-gallery-image')
+                .attr('src', $sel.data('image'));
+            disableButtons(counter, $sel.data('image-id'));
+        }
+
+        if (setIDs == true) {
+            $('[data-image-id]')
+                .each(function () {
+                    counter++;
+                    $(this)
+                        .attr('data-image-id', counter);
+                });
+        }
+        $(setClickAttr)
+            .on('click', function () {
+                updateGallery($(this));
+            });
+    }
+
+
 });
 $('.year').html(new Date().getFullYear());
+
--- webpack.config.js
+++ webpack.config.js
@@ -79,6 +79,14 @@ module.exports = {
       template: './src/index.html',
       filename: 'index.html',
     }),
+    new HtmlWebpackPlugin({
+      template: './src/projects.html',
+      filename: 'projects.html',
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/project-details.html',
+      filename: 'project-details.html',
+    }),
 
     new MiniCssExtractPlugin({ filename: 'css/style.css', }),
   ],
